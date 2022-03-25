4c85818be1d5c237feb60e4f1c26985badb2e19d
123Lea <leana.claude@students.gymneufeld.ch>
2022-03-25 11:36:03 +0100

    1. Version
    Co-authored-by: Michèle Wyss <michele.wyss@students.gymneufeld.ch>

---
+++ baum.py
@@ -0,0 +1,16 @@
+def greet ():
+    print("Hello")
+
+greet()
+
+def hallo():
+    print("Ciao")
+
+hallo()
+
+
+
+
+
+    
+
---
+++ game-kompakt.py
@@ -0,0 +1,16 @@
+git import fragen
+fragen.Level_1()
+fragen.Level_2()
+
+
+
+
+
+
+
+# LEVEL 2
+#false_counter = 0
+#skip_counter = 0
+
+
+
