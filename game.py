22b4addb61341d3e6498f63dcff4fe12fd2d467c
123lea <102354119+123lea@users.noreply.github.com>
2022-04-29 11:21:56 +0200

    Add files via upload

---
+++ game.py
@@ -0,0 +1,6 @@
+import levels
+import texte
+print(einleitung["text"])
+for level in range(0,6):
+    level_allg(levels[level])
+level_7()
---
+++ texte.py
@@ -0,0 +1,7 @@
+import version_2
+
+
+print(einleitung["text"])
+for level in range(0,6):
+    level_allg(levels[level])
+level_7()
